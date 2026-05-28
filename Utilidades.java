bddcb436b75432945929b569a45e484bb7a2a81b
ANAYANCY RIOS MORA <ANAYANCY RIOS MORA@DESKTOP-PN5LERG.lan>
2026-05-28 17:35:20 -0500

    Desarrollo de las clases, Persona,Direccion,TipoPersona,Utilidades

---
+++ Utilidades.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ANAYANCY RIOS MORA
+ */
+public class Utilidades {
+
+    public static void mostrarBienvenida() {
+        System.out.println("====================================");
+        System.out.println(" SISTEMA DE GESTIÓN ACADÉMICA ");
+        System.out.println("====================================");
+    }
+}
